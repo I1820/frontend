fd858ab35812553d12f31e46fcc0f85d8ee2069b
Mohammad <bugless.ir@yahoo.com>
2018-02-27 17:26:55 +0330

    Added nav and rtl modal

--- src/views/Projects/ProjectsList.js
+++ src/views/Projects/ProjectsList.js
@@ -39,20 +39,27 @@ class ProjectsList extends Component {
         return (
             <div>
 
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className="text-right">
+                    <ModalHeader>پروژه جدید</ModalHeader>
                     <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                        ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        <Form>
+                            <FormGroup row>
+                                <Label sm={3}>نام پروژه : </Label>
+                                <Col sm={9}>
+                                    <Input type="text" />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label sm={3}>توضیحات :‌ </Label>
+                                <Col sm={9}>
+                                    <Input type="textarea" name="" rows="2" />
+                                </Col>
+                            </FormGroup>
+                        </Form>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                        <Button color="primary" className="ml-1" onClick={this.toggle}>ذخیره</Button>
+                        <Button color="danger" onClick={this.toggle}>انصراف</Button>
                     </ModalFooter>
                 </Modal>
 
