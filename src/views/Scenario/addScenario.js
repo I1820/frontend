f46ca462d649a119442295852c7a3bcc0bfd0e50
pesher <sepehr.sabour@gmail.com>
2018-03-02 17:46:31 +0330

    Add map and editor

--- src/views/Scenario/addScenario.js
+++ src/views/Scenario/addScenario.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Card,
     CardHeader,
     CardBody,
     CardFooter,
     CardTitle,
-    Button,
+    Button, FormGroup, Form, Label, Col, Input,
 } from 'reactstrap';
 
+
+import AceEditor from 'react-ace';
+
+import 'brace/theme/monokai';
+import 'brace/mode/python';
+import 'brace/snippets/python';
+import 'brace/ext/language_tools';
 
 class AddScenario extends Component {
 
@@ -17,13 +24,41 @@ class AddScenario extends Component {
 
 
     render() {
-        return(
+        return (
             <div>
                 <Card className="text-justify">
                     <CardHeader>
                         <CardTitle className="mb-0 font-weight-bold h6">ویرایشگر سناریو</CardTitle>
                     </CardHeader>
                     <CardBody>
+                        <Form>
+                            <FormGroup row>
+                                <Label sm={1}>نام سناریو:</Label>
+                                <Col sm={5}>
+                                    <Input type="text"/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <AceEditor
+                                    mode="python"
+                                    theme="monokai"
+                                    className="col-md-12"
+                                    name="UNIQUE_ID_OF_DIV"
+                                    fontSize={14}
+                                    showPrintMargin={true}
+                                    showGutter={true}
+                                    highlightActiveLine={true}
+                                    editorProps={{$blockScrolling: true}}
+                                    setOptions={{
+                                        enableBasicAutocompletion: true,
+                                        enableLiveAutocompletion: true,
+                                        enableSnippets: true,
+                                        showLineNumbers: true,
+                                        tabSize: 2,
+                                    }}
+                                />
+                            </FormGroup>
+                        </Form>
                     </CardBody>
                     <CardFooter>
                         <Button className="ml-1" color="primary" size="md">ارسال سناریو</Button>
