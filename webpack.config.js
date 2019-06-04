e8d67ffd0c27a9eff51d765fc2ebce807c48ecb9
Parham Alvani <parham.alvani@gmail.com>
2019-06-05 03:06:07 +0430

    feat: Remove Comments from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,9 @@ module.exports = (env = {}) => {
       path: BUILD_DIR,
       filename: '[name].bundle.js'
     },
-    // watch: true,
     devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: BUILD_DIR,
-      //   port: 9001,
       compress: true,
       hot: true,
       open: true,
