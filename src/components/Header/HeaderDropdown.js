84b2fc2fefcef75f1d43f856c7e4164ac0e8c164
Parham Alvani <parham.alvani@gmail.com>
2019-06-11 00:27:08 +0430

    fix: Automatic Refresh on User Change

--- src/components/Header/HeaderDropdown.js
+++ src/components/Header/HeaderDropdown.js
@@ -19,6 +19,14 @@ class HeaderDropdown extends Component {
         }
     }
 
+    componentWillReceiveProps(props) {
+        if (props.userInfo !== undefined) {
+            this.setState({
+                activeUserInfo: props.userInfo,
+            })
+        }
+    }
+
     profileLink() {
         window.location = '#/profile'
     }
@@ -33,20 +41,20 @@ class HeaderDropdown extends Component {
         return (
             <UncontrolledDropdown nav>
                 <DropdownToggle nav>
-                    <img src={this.props.userInfo.picture} className="img-avatar" alt=""/>
+                    <img src={this.state.activeUserInfo.picture} className="img-avatar" alt=""/>
                 </DropdownToggle>
                 <DropdownMenu right>
                     <DropdownItem header className="text-center">
                         <strong>{this.state.activeUserInfo.username}</strong>
                     </DropdownItem>
-                    <DropdownItem onClick={this.profileLink}><i className="fa fa-user"></i>{'حساب کاربری'}
+                    <DropdownItem onClick={this.profileLink}><i className="fa fa-user"/>{'حساب کاربری'}
                     </DropdownItem>
                     {this.state.activeUserInfo.impersonated ?
                         <DropdownItem onClick={() => this.props.dispatch(impersonateUserAction(0, 0, toastAlerts))}>
-                            <i className="fa fa-power-off"></i>{'خروج از حالت شخص سوم'}
+                            <i className="fa fa-power-off"/>{'خروج از حالت شخص سوم'}
                         </DropdownItem> : ''}
                     <DropdownItem onClick={() => this.props.dispatch(logout())}><i
-                        className="fa fa-lock"></i>خروج</DropdownItem>
+    className="fa fa-lock"/>خروج</DropdownItem>
                 </DropdownMenu>
             </UncontrolledDropdown>
         )
